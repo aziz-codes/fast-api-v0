d3c32212f047ef537e0daa333726511a24926db9
aziz-codes <azizisoffline@gmail.com>
2026-05-07 01:25:42 +0500

    post route with body

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI
 from mock.users import users as user_data
+from pydantic import BaseModel
+
+
+class User(BaseModel):
+    name:str
+    age: int
 
 #fast api ref as app.
 app = FastAPI()
@@ -7,3 +13,13 @@ app = FastAPI()
 @app.get("/users")
 def users():
     return {"message":"Users route","data":user_data}
+
+@app.post("/users")
+def post_users(user:User):
+    payload = {
+        "id":len(user_data)+1,
+        "name": user.name,
+        "age": user.age
+    }
+    user_data.append(payload)
+    return{"message":"user created","data":payload}
